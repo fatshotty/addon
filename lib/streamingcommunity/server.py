2f8f3bb09b8ffd6cb239d918f312f5a5409d8bf9
fatshotty <fat@fatshotty.net>
2021-06-16 17:59:53 +0200

    cherrypick streamingcommunity

--- lib/streamingcommunity/server.py
+++ lib/streamingcommunity/server.py
@@ -1,78 +1,26 @@
-import sys, traceback, random
+import sys, traceback
 if sys.version_info[0] >= 3:
-    PY3 = True
-    from http.server import HTTPServer, BaseHTTPRequestHandler
+    from http.server import HTTPServer
     from socketserver import ThreadingMixIn
-    import urllib.request as urllib
-    import urllib.parse as urlparse
 else:
-    PY3 = False
-    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
     from SocketServer import ThreadingMixIn
-    import urlparse
-    import urllib
-
 
 from threading import Thread
-# from platformcode import logger
-
-IP = "127.0.0.1"
+from platformcode import logger
 
 
-def find_port():
-    return random.randint(8000,8099)
-
-
-
-class KODServer(ThreadingMixIn, HTTPServer):
+class Server(ThreadingMixIn, HTTPServer):
     daemon_threads = True
     timeout = 1
-    running = False
-    port = 0
-
-    def __init__(self):
-
-        self.start()
-
-
-
-    def start(self):
-        OK = False
-        choices = 3
-
-        if self.running == True:
-            # server is already running
-            return True
-
-        while choices > 0:
-            port = find_port()
-
-            try:
-                HTTPServer.__init__(self, (IP, port), None)
-                self._port = port
-                OK = True
-            except Exception as e:
-                logger.warn(port, 'is not a valid port, try again')
-
-            if OK:
-                logger.info('HTTPServer has found port:', port)
-                break # exit loop
-                pass
-
-            choices = choices - 1
-
-        if OK:
-            self._thread = Thread(target=self.run, name='KOD HTTP Server')
-            self._thread.daemon = self.daemon_threads
-            self._thread.start()
-
-        return OK
-
-
+    def __init__(self, address, handler, client):
+        HTTPServer.__init__(self,address,handler)
+        self._client = client
+        self.running=True
+        self.request = None
 
     def stop(self):
-        self.server_close()
-        self.running = False
+        self.running=False
 
     # def serve(self):
     #     while self.running:
@@ -82,15 +30,10 @@ class KODServer(ThreadingMixIn, HTTPServer):
     #             logger.error(traceback.format_exc())
 
     def run(self):
-        self.serve_forever()
-        self.running == True
-
+        t=Thread(target=self.serve_forever, name='HTTP Server')
+        t.daemon=self.daemon_threads
+        t.start()
 
     def handle_error(self, request, client_address):
         if not "socket.py" in traceback.format_exc():
-            # logger.error(traceback.format_exc())
-            pass
-
-
-
-KODServer()
+            logger.error(traceback.format_exc())

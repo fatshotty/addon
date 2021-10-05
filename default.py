10107467ba68fb31e2be2bac11656b684facbffd
fatshotty <fat@fatshotty.net>
2021-10-05 14:50:45 +0200

    try to use  ffmpeg/ffprobe

--- default.py
+++ default.py
@@ -17,13 +17,30 @@ try:
 except:
     pass
 from platformcode import config, logger
+import platform
 
 logger.info("init...")
+logger.info('python version {}'.format(sys.version))
 
 librerias = xbmc.translatePath(os.path.join(config.get_runtime_path(), 'lib'))
 sys.path.insert(0, librerias)
 
 from platformcode import launcher
+
+# logger.info('start ABI')
+# import abi
+# binmodule = abi.load("plugin.video.kod", "_core")
+# logger.info('END ABI {}'.format( dir(binmodule) ) )
+
+
+
+
+
+# container_video = av.open('http://techslides.com/demos/sample-videos/small.mp4')
+# logger.info( 'bitrate: {}'.format(container_video.bit_rate) )
+# video = container_video.streams.video[0]
+# logger.info( 'video {}'.format(video.codec_context.codec.name))
+
 
 if sys.argv[2] == "":
     launcher.start()
--- lib/ffmpeg/ffprobe.py
+++ lib/ffmpeg/ffprobe.py
@@ -1,13 +1,25 @@
 import subprocess,  os, platform
-from platformcode import logger
+from platformcode import logger, config
 import tempfile
+import xbmc, xbmcvfs
 import shutil
+
 
 from core import jsontools
 
 def ffprobe_path():
 
-  TEMP_DIR = '/data/local/tmp/'
+  executable = 'ffprobe_arm64'
+
+  xbmcfolder = xbmc.translatePath('special://xbmc')
+  temporaryfile = os.path.join(xbmcfolder, executable)
+  srcfile = os.path.join(config.get_runtime_path(), 'bin/{}'.format(executable) )
+  if not os.path.exists( temporaryfile ):
+      xbmcvfs.copy(srcfile, temporaryfile)
+  
+
+  logger.info('{} exists? {}'.format(temporaryfile, os.path.exists( temporaryfile ) ) )
+  
 
   # try:
   #     import xbmc
@@ -17,38 +29,38 @@ def ffprobe_path():
   #     TEMP_DIR = os.getenv("TEMP") or os.getenv("TMP") or os.getenv("TMPDIR")
   #     logger.info('use ENV for tempdir: {}'.format(TEMP_DIR) )
 
-  executable = 'ffmpeg_arm64'
-  # executable = os.path.join( dirname, '../../bin/ffprobe' )
-  # if platform.system() == 'Windows':
-  #   executable = '{}.exe'.format( executable )
-  # elif platform.system() == 'Android':
-  #   executable = '{}_arm64'.format( executable )
+  # 
+  # # executable = os.path.join( dirname, '../../bin/ffprobe' )
+  # # if platform.system() == 'Windows':
+  # #   executable = '{}.exe'.format( executable )
+  # # elif platform.system() == 'Android':
+  # #   executable = '{}_arm64'.format( executable )
 
-  # move file into tempfolder
-  logger.info('tempdir is: {}'.format(TEMP_DIR))
-  temporaryfile = os.path.join(TEMP_DIR, executable)
-  logger.info('tempfile is: {}'.format(temporaryfile))
+  # # move file into tempfolder
+  # logger.info('tempdir is: {}'.format(TEMP_DIR))
+  # temporaryfile = os.path.join(TEMP_DIR, executable)
+  # logger.info('tempfile is: {}'.format(temporaryfile))
 
-  if not os.path.exists(temporaryfile):
+  # if not os.path.exists(temporaryfile):
 
-    logger.info('executable temp not exists, copy to temp')
+  #   logger.info('executable temp not exists, copy to temp')
 
-    dirname = os.path.dirname(__file__)
-    dirname = dirname.replace(' ', '\ ')
+  #   dirname = os.path.dirname(__file__)
+  #   dirname = dirname.replace(' ', '\ ')
 
-    fullexepath = os.path.abspath( os.path.join( dirname, '../../bin/', executable ) )
+  #   fullexepath = os.path.abspath( os.path.join( dirname, '../../bin/', executable ) )
 
-    logger.info('Original exe is: {}'.format(fullexepath) )
+  #   logger.info('Original exe is: {}'.format(fullexepath) )
 
-    shutil.copy(fullexepath, temporaryfile)
+  #   shutil.copy(fullexepath, temporaryfile)
 
-  status = os.stat(temporaryfile)
-  permissions = oct(status.st_mode)
-  logger.info('current permission: {}'.format(permissions) )
-
-  # os.chmod(temporaryfile, 0o755)
   # status = os.stat(temporaryfile)
   # permissions = oct(status.st_mode)
+  # logger.info('current permission: {}'.format(permissions) )
+
+  os.chmod(temporaryfile, 0o755)
+  status = os.stat(temporaryfile)
+  permissions = oct(status.st_mode)
 
   logger.info('platform: {} - script: {} - perm: {}'.format(platform.system(), temporaryfile, permissions))
   
@@ -60,36 +72,54 @@ def get_metadata_from_url(url):
   
   # ffmpegpath = os.path.join(os.path.dirname(__file__), '../bin/ffprobe')
   # '/Users/fatshotty/Library/Application\ Support/Kodi/addons/plugin.video.kod/core/../bin/ffprobe'
-  ffmpegpath = ffprobe_path()
-  command = '{} -i "{}"'.format(ffmpegpath, url)
+  ffprobepath = ffprobe_path()
+  command = '{} -v quiet -print_format json -show_format -show_streams "{}"'.format(ffprobepath, url)
 
-  childproces = None
-  try:
-    childproces = subprocess.run('chmod +x {}'.format(ffmpegpath), shell=True, capture_output=True, check=True)
-    logger.info("RET-CODE-chmod: {} - {}".format(childproces.returncode, childproces.stdout.decode('utf-8')) )
-    status = os.stat(ffmpegpath)
-    permissions = oct(status.st_mode)
-    logger.info('new current permission: {}'.format(permissions) )
-  except Exception  as ex:
-    logger.error("Cannot change permission", ex)
+  command = [ffprobepath, '-version']
+  # try:
+  #   childproces = subprocess.run('chmod +x {}'.format(ffmpegpath), shell=True, capture_output=True, check=True)
+  #   logger.info("RET-CODE-chmod: {} - {}".format(childproces.returncode, childproces.stdout.decode('utf-8')) )
+  #   status = os.stat(ffmpegpath)
+  #   permissions = oct(status.st_mode)
+  #   logger.info('new current permission: {}'.format(permissions) )
+  # except Exception  as ex:
+  #   logger.error("Cannot change permission", ex)
 
+  # try:
+  #   logger.info("COMMAND new path: {}".format(command) )
+  #   childproces = subprocess.run(command, shell=True, capture_output=True, check=True)
+  #   logger.info("RET-CODE: {}".format(childproces.returncode) )
+  #   response = childproces.stdout
+  #   # logger.info(response)
+  #   responsestr = response.decode('utf-8')
+  #   logger.info("RET-STR: {}".format(responsestr) )
+  #   json = jsontools.load(responsestr)
+  #   return parse_response(json)
+  # except Exception  as ex:
+  #   if childproces == None:
+  #     logger.error("Cannot execute ffprobe", ex)
+  #   else:
+  #     logger.error("Cannot use ffprobe - exit code is: {}".format(childproces.returncode), ex )
+  #   raise ex
+  out = None
+  err = None
   try:
-    childproces = None
-    logger.info("COMMAND new path: {}".format(command) )
-    childproces = subprocess.run(command, shell=True, capture_output=True, check=True)
-    logger.info("RET-CODE: {}".format(childproces.returncode) )
-    response = childproces.stdout
-    # logger.info(response)
-    responsestr = response.decode('utf-8')
-    logger.info("RET-STR: {}".format(responsestr) )
-    json = jsontools.load(responsestr)
+    logger.info('executing {}'.format(command) )
+    out, err = subprocess.Popen(
+      [ffprobepath, '-version'],
+      stdin=subprocess.PIPE,
+      stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+    logger.info("RET-STR: {}".format(out) )
+    logger.info("RET-ERR: {}".format(err) )
+    json = jsontools.load(out)
     return parse_response(json)
-  except Exception  as ex:
-    if childproces == None:
-      logger.error("Cannot execute ffprobe", ex)
-    else:
-      logger.error("Cannot use ffprobe - exit code is: {}".format(childproces.returncode), ex )
-    raise ex
+  except Exception as e:
+    logger.error(e)
+
+    logger.info('out:', out)
+    logger.info('err:', err)
+
+    raise e
   
 
 
